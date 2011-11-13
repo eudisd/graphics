22deb2d1780b567172ca7d162e5720a76c78de3e
Eudis Duran <eudisd@gmail.com>
2011-11-13 14:58:16 -0500

    Fixed a few bugs regarding the projection matrix

--- rotation/arb.c
+++ rotation/arb.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 
-void init(void);
+void display(void);
 
 int main(int argc, char **argv){
 
@@ -11,13 +11,40 @@ int main(int argc, char **argv){
     glutInitWindowSize(640, 480);
     glutInitWindowPosition(0, 0);
     glutCreateWindow(argv[0]);
-   
-    init();
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45, 640.0/480.0, 1, 10);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+    glClearColor(0.0,0.0, 0.0, 1.0);
+    glutDisplayFunc(display);
 
     glutMainLoop();
     return EXIT_SUCCESS;
 }
 
-void init(void){
 
+void display(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(1.0, 0.0, 0.0);
+    glEnd();
+    glColor3f(0.0, 1.0, 0.0);
+    glBegin(GL_LINES);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, 1.0, 0.0);
+    glEnd();
+    glColor3f(0.0, 0.0, 1.0);
+    glBegin(GL_LINES);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, 0.0, 1.0);
+    glEnd();
+    //glFlush();
+    glutSwapBuffers();
 }
